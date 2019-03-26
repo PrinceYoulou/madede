ee60e2e9061854da444be17fe168e7465b36ce11
OrionWambert <wambertorion@gmail.com>
2019-03-26 23:03:59 +0100

    adding json object for portrait view

---
+++ js/portrait_data.js
@@ -0,0 +1,50 @@
+const data = [
+    {
+        "id": 1,
+        "pathToImage": "../img/agro1.jpg",
+        "firstName": "Luc",
+        "lastName": "MAKOUALA",
+        "fonction": "agriculteur",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod cumque excepturi eius sit illum ipsum, sint ullam temporibus accusantium tempore eum! Corrupti illum tempore optio molestiae nostrum culpa natus vero!",
+        "adresse": "12 rue Mandolina",
+        "telephone": "06 666 66 66 /05 555 55 55",
+        "localite": "Quartier Loboko, Bouendza"
+    },
+    {
+
+        "id": 2,
+        "pathToImage": "../img/agro1.jpg",
+        "firstName": "Jean",
+        "lastName": "ITOUA",
+        "fonction": "agriculteur",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod cumque excepturi eius sit illum ipsum, sint ullam temporibus accusantium tempore eum! Corrupti illum tempore optio molestiae nostrum culpa natus vero!",
+        "adresse": "12 rue Mandolina",
+        "telephone": "06 666 66 66 /05 555 55 55",
+        "localite": "Quartier Loboko, Bouendza"
+    },
+    {
+
+        "id": 3,
+        "pathToImage": "../img/agro1.jpg",
+        "firstName": "Helène",
+        "lastName": "NGABA",
+        "fonction": "agriculteur",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod cumque excepturi eius sit illum ipsum, sint ullam temporibus accusantium tempore eum! Corrupti illum tempore optio molestiae nostrum culpa natus vero!",
+        "adresse": "12 rue Mandolina",
+        "telephone": "06 666 66 66 /05 555 55 55",
+        "localite": "Quartier Loboko, Bouendza"
+    },
+    {
+
+        "id": 4,
+        "pathToImage": "../img/agro1.jpg",
+        "firstName": "Patrick",
+        "lastName": "MOSSALA",
+        "fonction": "agriculteur",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod cumque excepturi eius sit illum ipsum, sint ullam temporibus accusantium tempore eum! Corrupti illum tempore optio molestiae nostrum culpa natus vero!",
+        "adresse": "12 rue Mandolina",
+        "telephone": "06 666 66 66 /05 555 55 55",
+        "localite": "Quartier Loboko, Bouendza"
+
+    }
+]
