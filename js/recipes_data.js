1ae104bfccf5cacc192f029f5df5ca7df725e282
OrionWambert <wambertorion@gmail.com>
2019-03-26 23:08:23 +0100

    adding recipes object for recipes list view

---
+++ js/recipes_data.js
@@ -0,0 +1,127 @@
+const data = {
+    "dailyRecipe": {
+        "id": 0,
+        "titre": "Poulet à la patte d'arachide",
+        "pathToImage": "../img/main.jpg",
+        "temps": "2h",
+        "ingredients": [
+            {
+                "nom": "un ingredient"
+            },
+            {
+                "nom": "un autre ingredient"
+            }
+        ]
+    },
+
+    "recipes": [
+        {
+
+            "id": 1,
+            "titre": "Poulet à la patte d'arachide",
+            "pathToImage": "../img/recette1.jpg",
+            "temps": "2h",
+            "ingredients": [
+                {
+                    "nom": "un ingredient"
+                },
+                {
+                    "nom": "un autre ingredient"
+                }
+            ]
+        },
+        {
+
+            "id": 2,
+            "titre": "Poulet à la patte d'arachide",
+            "pathToImage": "../img/recette1.jpg",
+            "temps": "2h",
+            "ingredients": [
+                {
+                    "nom": "un ingredient"
+                },
+                {
+                    "nom": "un autre ingredient"
+                }
+            ]
+        },
+        {
+
+            "id": 3,
+            "titre": "Poulet à la patte d'arachide",
+            "pathToImage": "../img/recette1.jpg",
+            "temps": "2h",
+            "ingredients": [
+                {
+                    "nom": "un ingredient"
+                },
+                {
+                    "nom": "un autre ingredient"
+                }
+            ]
+        },
+        {
+
+            "id": 4,
+            "titre": "Poulet à la patte d'arachide",
+            "pathToImage": "../img/recette1.jpg",
+            "temps": "2h",
+            "ingredients": [
+                {
+                    "nom": "un ingredient"
+                },
+                {
+                    "nom": "un autre ingredient"
+                }
+            ]
+        }
+        ,
+        {
+
+            "id": 5,
+            "titre": "Poulet à la patte d'arachide",
+            "pathToImage": "../img/recette1.jpg",
+            "temps": "2h",
+            "ingredients": [
+                {
+                    "nom": "un ingredient"
+                },
+                {
+                    "nom": "un autre ingredient"
+                }
+            ]
+        }
+        ,
+        {
+
+            "id": 6,
+            "titre": "Poulet à la patte d'arachide",
+            "pathToImage": "../img/recette1.jpg",
+            "temps": "2h",
+            "ingredients": [
+                {
+                    "nom": "un ingredient"
+                },
+                {
+                    "nom": "un autre ingredient"
+                }
+            ]
+        }
+        ,
+        {
+
+            "id": 7,
+            "titre": "Poulet à la patte d'arachide",
+            "pathToImage": "../img/recette1.jpg",
+            "temps": "2h",
+            "ingredients": [
+                {
+                    "nom": "un ingredient"
+                },
+                {
+                    "nom": "un autre ingredient"
+                }
+            ]
+        }
+    ]
+}
