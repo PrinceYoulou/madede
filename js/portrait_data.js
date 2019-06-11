a28559818ca7510b75bf5c4273a2dcc178776ff0
Prince Youlou <you@example.com>
2019-06-11 12:24:13 +0100

    portrait images added to img folder and portraits infos integrated in js file

--- js/portrait_data.js
+++ js/portrait_data.js
@@ -1,50 +1,115 @@
 const data = [
     {
         "id": 1,
-        "pathToImage": "./img/agro1.jpg",
-        "firstName": "Luc",
-        "lastName": "MAKOUALA",
-        "fonction": "agriculteur",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod cumque excepturi eius sit illum ipsum, sint ullam temporibus accusantium tempore eum! Corrupti illum tempore optio molestiae nostrum culpa natus vero!",
-        "adresse": "12 rue Mandolina",
-        "telephone": "06 666 66 66 /05 555 55 55",
-        "localite": "Quartier Loboko, Bouendza"
+        "pathToImage": "./img/Beatrice Pambou.JPG",
+        "firstName": "Béatrice",
+        "lastName": "PAMBOU",
+        "fonction": "Productrice de haricots",
+        "description": "Présidente du groupement Songo Zola situé dans le district de Boko-Songho, Béatrice PAMBOU a pu, grâce aux revenus dégagés par la vente du haricot, électrifier sa maison avec l’achat d’un panneau solaire.",
+        "adresse": "/",
+        "telephone": "/",
+        "localite": "Groupement Songo Zola, District de Boko-Songho, Département de la Bouenza"
     },
     {
 
         "id": 2,
-        "pathToImage": "./img/agro1.jpg",
-        "firstName": "Jean",
-        "lastName": "ITOUA",
-        "fonction": "agriculteur",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod cumque excepturi eius sit illum ipsum, sint ullam temporibus accusantium tempore eum! Corrupti illum tempore optio molestiae nostrum culpa natus vero!",
-        "adresse": "12 rue Mandolina",
-        "telephone": "06 666 66 66 /05 555 55 55",
-        "localite": "Quartier Loboko, Bouendza"
+        "pathToImage": "./img/Eugenie Ngimbi.JPG",
+        "firstName": "Eugenie",
+        "lastName": "NGIMBI",
+        "fonction": "Productrice de haricots",
+        "description": "À 57 ans, Eugénie NGIMBI est responsable du groupement Femme Debout, un groupement exclusivement composé de femmes dans le district de Loudima. « Le haricot, c’est une affaire de famille. Mon rôle de Présidente dans le groupement, c’est d’encourager les autres pour la réussite du projet. Avant le projet, nous récoltions 100 à 150 kg de haricot, aujourd’hui, nous produisons 300, 500, 600, parfois même 1 000 kg.",
+        "adresse": "/",
+        "telephone": "/",
+        "localite": "Groupement Mikassou, District de Loudima, Département de la Bouenza"
     },
     {
 
         "id": 3,
-        "pathToImage": "./img/agro1.jpg",
-        "firstName": "Helène",
-        "lastName": "NGABA",
-        "fonction": "agriculteur",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod cumque excepturi eius sit illum ipsum, sint ullam temporibus accusantium tempore eum! Corrupti illum tempore optio molestiae nostrum culpa natus vero!",
-        "adresse": "12 rue Mandolina",
-        "telephone": "06 666 66 66 /05 555 55 55",
-        "localite": "Quartier Loboko, Bouendza"
+        "pathToImage": "./img/Pierre Kimbenga.JPG",
+        "firstName": "Pierre",
+        "lastName": "KIMBENGA",
+        "fonction": "Producteur de haricots",
+        "description": "Pierre est le Président du groupement Yengue Madiadia. Lors du premier cycle cultural, il a récolté plus de 400 kg de haricot « Grâce aux revenus supplémentaires dégagés par le vente du haricot, je peux assumer les frais de scolarité de mes enfants.",
+        "adresse": "/",
+        "telephone": "/",
+        "localite": "Groupement Yengue Madiadia, District de Boko-Songho, Département de la Bouenza"
     },
     {
 
         "id": 4,
-        "pathToImage": "./img/agro1.jpg",
-        "firstName": "Patrick",
-        "lastName": "MOSSALA",
-        "fonction": "agriculteur",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod cumque excepturi eius sit illum ipsum, sint ullam temporibus accusantium tempore eum! Corrupti illum tempore optio molestiae nostrum culpa natus vero!",
-        "adresse": "12 rue Mandolina",
-        "telephone": "06 666 66 66 /05 555 55 55",
-        "localite": "Quartier Loboko, Bouendza"
+        "pathToImage": "./img/Rodiate Mimounsi.JPG",
+        "firstName": "Rodiate",
+        "lastName": "MIMOUNSI",
+        "fonction": "Productrice de haricots",
+        "description": "Rodiate est membre du groupement Songa Zola présent dans le district de Boko-Songho. « Grâce au projet, nous pouvons soutenir, nos familles, nos amis, nos camarades.",
+        "adresse": "/",
+        "telephone": "/",
+        "localite": "Groupement Songa Zola, District de Boko-Songho, Département de la Bouenza"
+
+    }
+    {
+
+        "id": 5,
+        "pathToImage": "./img/avatar.jpg",
+        "firstName": "Jean",
+        "lastName": "Paul",
+        "fonction": "Producteur de haricots",
+        "description": "Membre du groupement Makaya-Makaya du village éponyme, Jean-Paul et les membres de son groupement agricole ont baptisé la variété de semences reçue « Ma Katherine. « C’est l’une des variétés qui a le plus produit ! ",
+        "adresse": "/",
+        "telephone": "/",
+        "localite": "Village Makaya-Makaya, District de Loudima, Département de la Bouenza"
+
+    }
+    {
+
+        "id": 6,
+        "pathToImage": "./img/Suzanne Bimpala.JPG",
+        "firstName": "Suzanne",
+        "lastName": "BIMPALA",
+        "fonction": "Productrice de haricots",
+        "description": "Suzanne Bimpala est Présidente du groupement Mikassou dans le district de Loudima. Vous pouvez la retrouver régulièrement au marché forain de Loudima, lieu où elle écoule une partie de sa production.",
+        "adresse": "/",
+        "telephone": "/",
+        "localite": "Groupement Songa Zola, District de Boko-Songho, Département de la Bouenza"
+
+    }
+    {
+
+        "id": 7,
+        "pathToImage": "./img/avatar.jpg",
+        "firstName": "Sisi",
+        "lastName": "MAKEDIKA",
+        "fonction": "Productrice de haricots",
+        "description": "« Grâce aux semences fournies, nous avons pu agrandir notre surface de culture et augmenter notre rendement »",
+        "adresse": "/",
+        "telephone": "/",
+        "localite": "Village Makaya-Makaya, District de Loudima, Département de la Bouenza"
+
+    }
+    {
+
+        "id": 8,
+        "pathToImage": "./img/avatar.jpg",
+        "firstName": "Médard"
+        "lastName": "X",
+        "fonction": "Producteur de haricots",
+        "description": "Médard fait partie du groupement Kibenza Zola 2 et travaille sur le site Missanga, il produit le haricot depuis deux décennies et complète ses revenus par le maraîchage. « Cela fait 20 ans que je produis le haricot. Le projet me permet d’augmenter la production par rapport à avant ».",
+        "adresse": "/",
+        "telephone": "/",
+        "localite": "Groupement Kibenza Zola 2, District de Boko-Songho, Département de la Bouenza"
+
+    }
+    {
+
+        "id": 9,
+        "pathToImage": "./img/avatar.jpg",
+        "firstName": "François"
+        "lastName": "LOUKANOU",
+        "fonction": "Producteur de haricots",
+        "description": "François produit du haricot depuis de nombreuses années, membre du groupement Songa Zola, l’entraide entre producteurs/trices fait partie des forces du groupement.",
+        "adresse": "/",
+        "telephone": "/",
+        "localite": "Groupement Songa Zola, District de Boko-Songho, Département de la Bouenza"
 
     }
 ]
